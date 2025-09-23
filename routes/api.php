d1a9a015bd30b37a4690f40c70bba9345aed0871
joaogabrielfossadeoliveira <joaogabrielfo24@gmail.com>
2025-09-23 13:44:54 -0300

    we

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
+use App\Livewire\SensorList;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/sensorlist', SensorList::class)->name('sensor.list');
