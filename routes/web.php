87635c36b7284df240cd4e7ec83799f9ca3cb835
joaogabrielfo <joaogabrielfo24@gmail.com>
2025-09-30 15:15:00 -0300

    kkk

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,23 @@ use App\Livewire\AmbienteList;
 use App\Livewire\Dashboard;
 
 use App\Livewire\Registro\RegistroList;
+use App\Models\Sensor;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/list/Registro ', RegistroList::class)->name('lista');
 
-use App\Models\Ambiente;
-use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', Dashboard::class);
 
-Route::get('/ambientecreate', AmbienteCreate::class)->name('ambiente.create');
+Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
 
 Route::get('/ambiente/{id}/edit', AmbienteEdit::class)->name('ambiente.edit');
 
-Route::get('/ambientelist', AmbienteList::class)->name('ambiente.list');
+Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
+
+
+//sensores
+
 
